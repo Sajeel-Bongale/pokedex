20733b1f007749406fd6481d3e48a408c0992def
Sajeel-Bongale <sajeelbongale@gmail.com>
2020-12-31 16:15:50 +0530

    PKDX | Adds error component to handle 404 routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import {ErrorComponent} from './components/error/error.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     redirectTo: 'home'
+  },
+  {
+    path: '**',
+    component: ErrorComponent
   }
 ];
 
